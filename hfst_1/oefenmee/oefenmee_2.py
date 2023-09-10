99fae41e73d8d7a49ea51e3060ec7585aa9d1e92
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2023-09-10 16:24:00 +0200

    Fouten uit oefenmee 2 gehaald.

--- hfst_1/oefenmee/oefenmee_2.py
+++ hfst_1/oefenmee/oefenmee_2.py
@@ -1,5 +1,5 @@
 # Start de oefen mee met onderstaande dictionary.
-fruitmand = { # Sleutel is fruit, element is aantal
+fruitmand = { # Sleutel is fruit, waarde is aantal
     "appel": 5,
     "banaan": 3,
     "kers": 50
