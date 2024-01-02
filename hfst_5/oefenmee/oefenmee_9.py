c161e2a42b8ed304c69f1f5c8c8804e60d03a956
HendrikClijstersMosa <hendrik.clijsters@mosa-rt.be>
2024-01-02 17:24:08 +0100

    Oefeningen van 2de deel OOP toegevoegd.

---
+++ hfst_5/oefenmee/oefenmee_9.py
@@ -0,0 +1,25 @@
+# Maak de klasse Hond aan zoals omschreven in oefen mee 9.
+
+
+
+" Via onderstaande code kan je niveau 1 testen. "
+
+# hond_1 = Hond("Lulu", "keuken")
+# hond_2 = Hond("Floris", "keuken")
+# hond_3 = Hond("Ranger", "tuin")
+
+# hond_1.ziet_hond(hond_2) # Lulu ziet Floris in de keuken.
+# hond_1.ziet_hond(hond_3) # Lulu ziet Ranger niet in de keuken.
+
+
+" Via onderstaande code kan je niveau 2 & 3 testen. Opgelet! Resultaat is random. "
+
+# hond_1 = Hond("Lulu", "keuken")
+# hond_2 = Hond("Floris", "keuken")
+# hond_3 = Hond("Ranger", "tuin")
+
+# hond_1.ziet_hond(hond_2) # Lulu ziet Floris in de keuken.
+#                          # Lulu is bang en rent naar de tuin.
+# hond_1.ziet_hond(hond_3) # Lulu ziet Ranger in de tuin.
+#                          # Lulu is bang en rent naar de buren.
+# print(hond_1.locatie)    # buren
