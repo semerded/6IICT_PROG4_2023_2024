ed7a441126c0b4066fd578b92a0f07d10eedb8e2
semerded <sem.van.broekhoven@gmail.com>
2024-01-15 18:08:03 +0100

    added color change on wall bounce

--- hfst_5/opdrachten/opdracht_7/particles/bouncing_particle.py
+++ hfst_5/opdrachten/opdracht_7/particles/bouncing_particle.py
@@ -1,14 +1,25 @@
 from particles.base_particle import BaseParticle
-import constants
+from colors import Colors
+from random import randint
+import constants, globals
+
+bouncingColors: list[globals.RGBcolor] = [Colors.YELLOW, Colors.BLUE]
 
 class BouncingParticle(BaseParticle):
     def __init__(self, speed: int) -> None:
-        super().__init__(speed)
+        self.currentColor = randint(0, 1)
+        super().__init__(speed, bouncingColors[self.currentColor])
         
     def place(self):
         self.basePlace()
         if self.currentPostition[0] < 5 or self.currentPostition[0] > constants.BREEDTE - 5:
             self.xMovement *= -1
+            self._changeColorWhenWallTouched()
         if self.currentPostition[1] < 5 or self.currentPostition[1] > constants.HOOGTE - 5:
             self.yMovement *= -1
-                
+            self._changeColorWhenWallTouched()
+            
+    def _changeColorWhenWallTouched(self):
+        self.currentColor = 0 if self.currentColor == 1 else 1
+        self.changeColor(bouncingColors[self.currentColor])
+            
